61f2675802fae33009d0366c492b54875e1b0f78
Azizan Nur Rohman <zhiz.nevermind@gmail.com>
2022-07-31 12:16:17 +0700

    upload app

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::resource('/posts', controller: \App\Http\Controllers\PostController::class);
+Route::resource('/posts', \App\Http\Controllers\PostController::class);
+Route::delete('posts/{post}',[PostController::class,'destroy'])->name('posts.destroy');
